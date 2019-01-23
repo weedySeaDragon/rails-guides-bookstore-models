c2806a61dbe4520cbb2fdee38a0e614588360b25
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-01-23 01:34:29 -0800

    double -> single quotes; spaces for readability

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ApplicationRecord
   belongs_to :author, dependent: :destroy
 
   has_many :reviews
-  has_and_belongs_to_many :orders, join_table: "books_orders"
+  has_and_belongs_to_many :orders, join_table: 'books_orders'
 
 
   # default_scope { where('year_published >= ?', Date.current.year - 50 )}
@@ -12,11 +12,11 @@ class Book < ApplicationRecord
   scope :in_print, -> { where(out_of_print: false) }
   scope :out_of_print, -> { where(out_of_print: true) }
 
-  scope :recent, -> { where('year_published >= ?', Date.current.year - 50 )}
-  scope :old, -> { where('year_published < ?', Date.current.year - 50 )}
+  scope :recent, -> { where('year_published >= ?', Date.current.year - 50) }
+  scope :old, -> { where('year_published < ?', Date.current.year - 50) }
 
-  scope :out_of_print_and_expensive, -> { out_of_print.where("price > 500") }
+  scope :out_of_print_and_expensive, -> { out_of_print.where('price > 500') }
 
-  scope :costs_more_than, ->(amount) { where("price > ?", amount) }
+  scope :costs_more_than, ->(amount) { where('price > ?', amount) }
 
 end
