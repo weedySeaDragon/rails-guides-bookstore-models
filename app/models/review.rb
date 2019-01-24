0158f9e02b4bf0818f0684ed67dd913933900391
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-01-23 23:42:26 -0800

    whitespace formatting: spacing before/after enum brackets

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :customer
   belongs_to :book
 
-  enum state: [:not_reviewed, :published, :hidden]
+  enum state: [ :not_reviewed, :published, :hidden ]
 end
