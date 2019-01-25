2630007da104ce7016c8a9182403ddf09c479430
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-01-24 16:01:19 -0800

    don't destroy authors just because a book is destroyed

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
 
   belongs_to :supplier
-  belongs_to :author, dependent: :destroy
+  belongs_to :author
 
   has_many :reviews
   has_and_belongs_to_many :orders, join_table: 'books_orders'
