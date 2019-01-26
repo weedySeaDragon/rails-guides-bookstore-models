89000be59432abcb27c7054eb699be6e154bb6e8
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-01-25 18:50:02 -0800

    delete old seeder files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-# require all files in ./seeders and below:
-Dir[File.join(__dir__, 'seeders', '**', '*.rb')].each { |f| require f }
-
 require_relative File.join(__dir__,'seeders', 'csv_data_loader')
 
 CSVDataLoader.load_csv_data
