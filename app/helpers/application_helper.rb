8c627aa7c177c1d5333fc9b26e253a609ba555a5
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-01-25 17:37:40 -0800

    external link helper method and style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,13 @@ module ApplicationHelper
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 
+
+  # this is so wrong. 2 links.  yuck.
+  def extern_link(text, url)
+
+    link_to(text, url) +
+        link_to(content_tag(:sup, icon('fas', 'external-link-alt', class: 'external-link')),
+                url)
+  end
+
 end
