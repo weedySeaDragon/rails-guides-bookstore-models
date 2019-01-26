058907346ac4dadf1eebf59cd2e50203369003d8
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-01-25 18:00:51 -0800

    helpers for Rails guides URLs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  RAILS_GUIDES_URL  = "https://guides.rubyonrails.org/"
+  AR_QUERYING_GUIDE = 'active_record_querying.html'
+
+
   def bootstrap_class_for(flash_type)
     {
         success: "alert-success",
@@ -10,12 +14,26 @@ module ApplicationHelper
   end
 
 
-  # this is so wrong. 2 links.  yuck.
+  # this is so wrong. 2 links. yuck.
   def extern_link(text, url)
 
-    link_to(text, url) +
+    link_to(text, url, target: '_blank', rel: 'no-follow') +
         link_to(content_tag(:sup, icon('fas', 'external-link-alt', class: 'external-link')),
-                url)
+                url, target: '_blank', rel: 'nofollow')
   end
 
+
+  def ar_querying_guide_url
+    rails_guide(AR_QUERYING_GUIDE)
+  end
+
+
+  def rails_guides_url
+    RAILS_GUIDES_URL
+  end
+
+
+  def rails_guide(guide_url)
+    RAILS_GUIDES_URL + guide_url
+  end
 end
