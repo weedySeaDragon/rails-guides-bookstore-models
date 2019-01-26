3962ab6702959d4e995c0deea506632fe44acc0d
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-01-25 18:31:43 -0800

    seed using the CSVDataLoader

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,6 @@
 # require all files in ./seeders and below:
 Dir[File.join(__dir__, 'seeders', '**', '*.rb')].each { |f| require f }
 
+require_relative File.join(__dir__,'seeders', 'csv_data_loader')
 
-SuppliersSeeder.seed(12)
-BooksSeeder.seed(40, { suppliers: Supplier.all })
-CustomersSeeder.seed
-OrdersSeeder.seed
-
-ReviewsSeeder.seed(105, { customers: Customer.all, books: Book.all })
-
-models = [Book, Customer, Order, Review, Supplier]
-puts "\nDatabase is now seeded with:"
-models.each do |model|
-  puts " #{model.count.to_s.rjust(4)} #{model.name.pluralize}"
-end
+CSVDataLoader.load_csv_data
